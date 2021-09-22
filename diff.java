8fe79eaca95ddb2453027f615dfd7ea5de9e069d
abhishek sharma <abhishek309200@gmail.com>
2021-09-22 19:28:15 +0530

    adding diff.java

---
+++ diff.java
@@ -0,0 +1,9 @@
+public class difference
+{  
+public static void main(String args[])   
+{  
+int n1 = 225, n2 = 115, sum;  
+diff = n1- n2;  
+System.out.println("The diff of numbers is= the diff is "+diff);  
+}  
+}  
